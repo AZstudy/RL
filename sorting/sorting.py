c9c124060885e223e005ee01ba3d23a185304156
Sukwon Choi <swchoi06@wafflestudio.com>
2017-07-01 14:39:30 +0900

    Edited sorting.py

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -15,8 +15,8 @@ class Sort:
                 if k == tuple(range(self.num_of_digits)): # Terminal Node
                     self.values[k] = 10.0
                     continue
-                value = -self.num_of_digits**2
-                for i, j in it.combinations(k, 2):
+                value = 0
+                for i, j in it.combinations(range(self.num_of_digits), 2):
                     k_list = list(k)
                     k_list[i], k_list[j] = k_list[j], k_list[i]
                     value = max(value, self.discount*self.old_values[tuple(k_list)])
